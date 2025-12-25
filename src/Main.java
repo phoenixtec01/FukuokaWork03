2e0462451232faf5fd259f93984d8b8d2697899f
Phoenix-Fukuoka <meets@phoenix-tecs.com>
2025-12-25 13:34:27 +0900

    Main追加

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			throw new AllException(AllException.ErrorCode.ALLOWABLENUM_ERROR);
+			new CommandParse(args);
 		} catch (AllException e) {
-			System.out.println("模擬出力：" + e.getErrorMessage());
+			System.out.println(e.getErrorMessage());
 		}
 	}
 
