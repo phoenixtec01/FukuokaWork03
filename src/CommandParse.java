fc7452c4bc8f2d87ea636cfcbf6e5202035a19b2
Phoenix-Fukuoka <meets@phoenix-tecs.com>
2025-12-26 16:49:22 +0900

    CommandParse更新

--- src/CommandParse.java
+++ src/CommandParse.java
@@ -9,27 +9,31 @@ public class CommandParse {
 	private List<Object> argsList = new ArrayList<>();
 
 	public CommandParse(String[] args, Map<String, CommandOption> optionMap) throws AllException {
-		for (String arg : args) {
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+
 			if (arg.startsWith("-")) {
 				option = optionMap.get(arg);
+
 			} else {
+
 				argsList.add(arg);
+
+				int afterCount = args.length - (i + 1); //現在地より後ろの引数を数える
+
+				if ("入荷予定".equals(arg) || "出荷予定".equals(arg)) {
+					if (afterCount != 4) {
+						throw new AllException(AllException.ErrorCode.PARAMETER_ERROR);
+					}
+				}
+
+				if ("入庫".equals(arg) || "出庫".equals(arg)) {
+					if (afterCount != 1) {
+						throw new AllException(AllException.ErrorCode.PARAMETER_ERROR);
+					}
+				}
 			}
 		}
-
-		boolean result = true; //false=例外出力
-		for (int i = 0; i < args.length; i++) {
-	        int afterCount = args.length - (i + 1);
-	        if ("入荷予定".equals(args[i])) {
-		        if (afterCount != 4) {
-		        	result = false;
-		        }
-		        break;
-		    }
-		}
-				
-		if (!result) {
-			throw new AllException(AllException.ErrorCode.PARAMETER_ERROR);
-		}
+		System.out.println(argsList);
 	}
 }
